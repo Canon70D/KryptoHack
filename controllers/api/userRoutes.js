be74f1e934b15158e40c03ea833de6b94b27291c
Ben Smerd <bsmerd@adam.com.au>
2022-09-12 21:01:59 +0930

    update

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -16,6 +16,9 @@ router.get('/profile/:id', async (req, res) => {
   try {
     const userProfile = await User.findByPk(req.params.id);
     const user = userProfile.map((project) => project.get({ plain: true }));
+
+    
+
   } catch (error) {
     res.status(500).json(error)
   }
