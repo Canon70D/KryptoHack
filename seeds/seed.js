7d6c6ad1dc208d4c3698bf2fdaea457523c9c873
Ben Smerd <bsmerd@adam.com.au>
2022-09-12 18:47:16 +0930

    update

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,24 +1,29 @@
 const sequelize = require("../config/connection");
 
-//const { User } = require('../models');
-//const userData = require("./userData.json");
-const userSeeds = require("./userSeeds");
+const { User, Crypto } = require("../models");
+const userData = require("./userData.json");
+const cryptoData = require('./cryptoSeed.json');
+//const userSeeds = require("./userSeeds");
 
 const seedDatabase = async () => {
-  // await sequelize.sync({ force: true });
-
-  // await User.bulkCreate(userData, {
-  //   individualHooks: true,
-  //   returning: true,
-  // });
-
-  // process.exit(0);
-
   await sequelize.sync({ force: true });
 
+  await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  await Crypto.bulkCreate(cryptoData, {
+    individualHooks: true,
+    returning: true,
+  });
+  process.exit(0);
+
+  // await sequelize.sync({ fource: true });
+
   console.log("-----------database seed-----------");
-  await userSeeds();
-  console.log("-----------users seeded----------");
+  // await userSeeds();
+  // console.log("-----------users seeded----------");
 };
 
 seedDatabase();
