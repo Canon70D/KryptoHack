4393a8d6688e26edf11e66dbdea01bc38c97195d
FEIYU YIN <105698932+Canon70D@users.noreply.github.com>
2022-09-14 16:06:15 +0930

    comment route

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,12 +1,50 @@
-const router = require('express').Router();
-const { Comment } = require('../../models');
+const router = require("express").Router();
+const { Comment } = require("../../models");
+const withAuth = require("../../utils/auth");
 
 // Get all comments by the crypto id and then they can output all the comments through the dom etc
 // will need to reference it through the crypto id model
-router.get('/', (req, res) => {
-    res.send("comment")
-})
+//(not sure how this could be done now since we dont have crypto id saved anymore ... )
+router.get("/", (req, res) => {
+  //   res.send("comment");
+  Comment.findAll()
+    .then((commentData) => res.json(commentData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
+router.post("/", withAuth, (req, res) => {
+  Comment.create({
+    comment_text: req.body.comment,
+    user_id: req.session.user_id,
+    coin_id: req.body.post_id,
+  })
+    .then((commentData) => res.json(commentData))
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+});
 
+router.delete("/:id", withAuth, (req, res) => {
+  Comment.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((commentData) => {
+      if (!commentData) {
+        res.status(404).json({ message: "not a valid comment ID" });
+        return;
+      }
+      res.json(commentData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 module.exports = router;
--- models/index.js
+++ models/index.js
@@ -2,17 +2,17 @@ const User = require("./User");
 const Comment = require("./Comment");
 // const Crypto = require("./Crypto");
 // const UserCrypto = require("./UserCrypto");
-const CoinList = require("./CoinList")
-const Favorites = require("./Favorites")
-
+const CoinList = require("./CoinList");
+const Favorites = require("./Favorites");
 
 //user to comment associations
 User.hasMany(Comment, { foreignKey: "user_id", onDelete: "set null" });
 Comment.belongsTo(User, { foreignKey: "user_id", onDelete: "set null" });
 
-//comment to crypto associations
-// Crypto.hasMany(Comment, { foreignKey: "crypto_id", onDelete: "set null" });
-// Comment.belongsTo(Crypto, { foreignKey: "crypto_id", onDelete: "set null" });
+// comment to crypto associations
+// we need to figure how to make this work without save the crypto to database, the id would be null
+// Crypto.hasMany(Comment, { foreignKey: "coin_id" });
+// Comment.belongsTo(Favorites, { foreignKey: "coin_id", onDelete: "set null" });
 
 // //user to crypto associations
 // User.belongsToMany(Crypto, {
